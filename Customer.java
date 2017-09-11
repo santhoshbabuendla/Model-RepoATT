2aa352a59d9eeb3e293bfbe3664cc7c69fc39c04
santosh <santhoshbabu574@gmail.com>
2017-09-10 22:14:47 -0400

    customer added

---
+++ Customer.java
@@ -0,0 +1,39 @@
+package com.advjava.custdetails.model;
+
+public class Customer {
+	
+	String name;
+	String bankName;
+	String accNumber;
+	String location;
+	
+		public Customer(String string, String string2, String string3, String string4) {
+		// TODO Auto-generated constructor stub
+	}
+		public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	public String getAccNumber() {
+		return accNumber;
+	}
+	public void setAccNumber(String accNumber) {
+		this.accNumber = accNumber;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+
+}
